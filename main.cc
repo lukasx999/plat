1c76515afbca01ada077146def8d8e983e3304cb
lukasx999 <116069013+lukasx999@users.noreply.github.com>
2025-06-12 15:56:00 +0200

    proper movement

--- main.cc
+++ main.cc
@@ -13,61 +13,62 @@ enum class Direction {
 
 class Player {
     Vector2 m_position;
-    Vector2 m_velocity;
-    Vector2 m_acceleration;
+    bool m_is_jumping;
+    float m_speed = 0;
     static constexpr int m_size = 100;
+    static constexpr int m_gravity = 1200;
+    static constexpr float m_movement_speed = 600;
+    static constexpr float m_jumping_speed = 900;
 
 public:
     Player()
         : m_position(WIDTH/2.0f, HEIGHT-m_size/2.0f)
-        , m_velocity(0, 0)
-        , m_acceleration(0, 0)
+        , m_is_jumping(false)
     { }
 
-    [[nodiscard]] bool is_on_floor() const {
-        return m_position.y == HEIGHT-m_size/2.0f;
+    [[nodiscard]] bool is_grounded() const {
+        return m_position.y >= HEIGHT-m_size/2.0f;
     }
 
     void draw() const {
-        DrawRectanglePro({ m_position.x, m_position.y, m_size, m_size }, { m_size/2.0f, m_size/2.0f }, 0, BLUE);
+        DrawRectanglePro(
+            { m_position.x, m_position.y, m_size, m_size },
+            { m_size/2.0f, m_size/2.0f },
+            0,
+            BLUE
+        );
         DrawCircleV(m_position, 10, RED);
-        DrawLineEx(m_position, Vector2Add(m_position, Vector2Scale(m_velocity, 100.0f)), 5, RED);
-        DrawLineEx(m_position, Vector2Add(m_position, Vector2Scale(m_acceleration, 100.0f)), 5, GREEN);
     }
 
-    void update() {
-        m_velocity = Vector2Add(m_velocity, m_acceleration);
-        m_position = Vector2Add(m_position, m_velocity);
-
-        if (is_on_floor()) {
-            m_velocity.y = 0;
-            m_acceleration.y = 0;
-        } else {
-            // gravity
-            m_acceleration.y += 1;
-        }
-
+    void update(float dt) {
         m_position.y = Clamp(m_position.y, 0, HEIGHT-m_size/2.0f);
         m_position.x = Clamp(m_position.x, 0, WIDTH-m_size/2.0f);
 
-        std::println("-------------------");
-        std::println("m_position: {}, {}", m_position.x, m_position.y);
-        std::println("m_velocity: {}, {}", m_velocity.x, m_velocity.y);
-        std::println("m_acceleration: {}, {}", m_acceleration.x, m_acceleration.y);
+        if (m_is_jumping) {
+            m_position.y += m_speed * dt;
+            m_speed += m_gravity * dt;
+        }
+
+        if (is_grounded()) {
+            m_is_jumping = false;
+        }
+
     }
 
     void jump() {
-        m_acceleration.y = -10;
+        if (m_is_jumping) return;
+        m_is_jumping = true;
+        m_speed = -m_jumping_speed;
     }
 
-    void move(Direction dir) {
+    void move(Direction dir, float dt) {
         switch (dir) {
             case Direction::Right:
-                m_acceleration.x += 0.01;
-            break;
+                m_position.x += m_movement_speed * dt;
+                break;
             case Direction::Left:
-                m_acceleration.x -= 0.01;
-            break;
+                m_position.x -= m_movement_speed * dt;
+                break;
         }
     }
 
@@ -85,21 +86,22 @@ int main(void) {
         BeginDrawing();
         {
             ClearBackground(BLACK);
-            player.draw();
-            player.update();
 
             if (IsKeyPressed(KEY_SPACE)) {
-                std::println("jump");
                 player.jump();
             }
 
             if (IsKeyDown(KEY_D)) {
-                player.move(Direction::Right);
+                player.move(Direction::Right, GetFrameTime());
             }
 
             if (IsKeyDown(KEY_A)) {
-                player.move(Direction::Left);
+                player.move(Direction::Left, GetFrameTime());
             }
+
+            DrawFPS(0, 0);
+            player.update(GetFrameTime());
+            player.draw();
 
         }
         EndDrawing();
