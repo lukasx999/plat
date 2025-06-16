5e8d743e9dbf352c59f1878cd9cd889177e2d69d
lukasx999 <116069013+lukasx999@users.noreply.github.com>
2025-06-16 23:43:13 +0200

    refactoring

--- main.cc
+++ main.cc
@@ -31,23 +31,154 @@ void draw_environment(const std::span<const Item> items) {
     }
 }
 
+enum class MovementDirection { Left, Right };
 
-
-
-
-class Player {
-    enum class MovementDirection { Left, Right };
-
+class PhysicsEntity {
     float m_speed = 0;
     bool m_is_grounded = false;
-    Rectangle m_origin;
     Vector2 m_position;
-    const std::function<float()> m_get_dt;
-    const Texture2D m_tex;
-    MovementDirection m_direction = MovementDirection::Left;
+    float m_width;
+    float m_height;
     static constexpr int m_gravity = 1000;
     static constexpr float m_movement_speed = 500;
     static constexpr float m_jumping_speed = 900;
+
+public:
+    MovementDirection m_direction = MovementDirection::Left;
+
+    PhysicsEntity(Vector2 position)
+    : m_position(position)
+    { }
+
+    virtual void update() {
+        if (m_is_grounded) {
+            m_speed = 0;
+        } else {
+            m_speed += m_gravity * GetFrameTime();
+            m_position.y += m_speed * GetFrameTime();
+        }
+    }
+
+    virtual void jump() {
+        if (!m_is_grounded) return;
+        m_speed = -m_jumping_speed;
+    }
+
+    void set_width(float width) {
+        m_width = width;
+    }
+
+    void set_height(float height) {
+        m_height = height;
+    }
+
+    virtual void move(MovementDirection direction) {
+        m_direction = direction;
+        switch (direction) {
+            case MovementDirection::Left:
+                m_position.x -= m_movement_speed * GetFrameTime();
+                break;
+            case MovementDirection::Right:
+                m_position.x += m_movement_speed * GetFrameTime();
+                break;
+        }
+    }
+
+    [[nodiscard]] Rectangle hitbox() const {
+        return {
+            m_position.x - m_width/2.0f,
+            m_position.y - m_height/2.0f,
+            m_width,
+            m_height,
+        };
+    }
+
+    virtual void resolve_collisions(const std::span<const Item> items) {
+        m_is_grounded = false;
+
+        for (const auto &item : items) {
+            if (item.m_is_blocking) {
+
+                const auto item_hitbox = item.m_hitbox;
+                const float delta_ver = m_speed * GetFrameTime();
+                const float delta_hor = m_movement_speed * GetFrameTime();
+
+                // let the player clip a bit into the floor when grounded, to prevent
+                // oscillation of grounding state
+                // also prevent the player from teleporting down after walking off a ledge
+                const float clip = 1;
+
+                const Rectangle left = {
+                    item_hitbox.x - delta_hor,
+                    item_hitbox.y + clip,
+                    delta_hor,
+                    item_hitbox.height - clip*2,
+                };
+
+                const Rectangle right = {
+                    item_hitbox.x + item_hitbox.width,
+                    item_hitbox.y + clip,
+                    delta_hor,
+                    item_hitbox.height - clip*2,
+                };
+
+                const float top_delta_mult_factor = 1.3;
+                const Rectangle top = {
+                    // remove some padding from each edge to prevent
+                    // instant teleportation when jumping up and edge
+                    // and moving to the right
+                    item_hitbox.x + delta_hor*top_delta_mult_factor,
+                    item_hitbox.y - delta_ver,
+                    item_hitbox.width - delta_hor*top_delta_mult_factor*2,
+                    delta_ver,
+                };
+
+                const Rectangle bottom = {
+                    item_hitbox.x,
+                    item_hitbox.y + item_hitbox.height,
+                    item_hitbox.width,
+                    // when the player bumps their head by jumping, the
+                    // speed is negative, therefore it must be inverted
+                    -delta_ver,
+                };
+
+#ifdef DEBUG_COLLISIONS
+                DrawRectangleRec(left,   PURPLE);
+                DrawRectangleRec(right,  PURPLE);
+                DrawRectangleRec(top,    PURPLE);
+                DrawRectangleRec(bottom, PURPLE);
+#endif // DEBUG_COLLISIONS
+
+                if (CheckCollisionRecs(hitbox(), top)) {
+                    m_is_grounded = true;
+                    m_position.y = item_hitbox.y - hitbox().height/2.0f + clip;
+                }
+
+                if (CheckCollisionRecs(hitbox(), left)) {
+                    m_position.x = item_hitbox.x - hitbox().width/2.0f;
+                }
+
+                if (CheckCollisionRecs(hitbox(), right)) {
+                    m_position.x = item_hitbox.x + item_hitbox.width + hitbox().width/2.0f;
+                }
+
+                if (CheckCollisionRecs(hitbox(), bottom)) {
+                    m_speed = 0;
+                }
+
+            }
+
+        }
+
+    }
+
+};
+
+
+class Player : public PhysicsEntity {
+    Rectangle m_origin;
+    const std::function<float()> m_get_dt;
+    const Texture2D m_tex;
     static constexpr std::array m_sprites_running {
         Rectangle { 8,   74,  14, 18 },
         Rectangle { 41,  74,  13, 18 },
@@ -72,19 +203,21 @@ class Player {
 
 public:
     Player(Vector2 position, std::function<float()> get_dt)
-        : m_origin(m_sprites_running[0])
-        , m_position(position)
+        : PhysicsEntity(position)
+        , m_origin(m_sprites_running[0])
         , m_get_dt(get_dt)
         , m_tex(LoadTexture(m_tex_path))
     { }
 
-    [[nodiscard]] Rectangle get_hitbox() const {
-        return {
-            m_position.x - m_origin.width*m_texture_scale/2.0f,
-            m_position.y - m_origin.height*m_texture_scale/2.0f,
-            m_origin.width*m_texture_scale,
-            m_origin.height*m_texture_scale,
-        };
+    void update() override {
+        PhysicsEntity::update();
+        set_width(m_origin.width*m_texture_scale);
+        set_height(m_origin.height*m_texture_scale);
+    }
+
+    void move(MovementDirection direction) override {
+        PhysicsEntity::move(direction);
+        walk();
     }
 
     void draw() const {
@@ -101,7 +234,7 @@ public:
                 break;
         }
 
-        DrawTexturePro(m_tex, origin, get_hitbox(), { 0, 0 }, 0, WHITE);
+        DrawTexturePro(m_tex, origin, hitbox(), { 0, 0 }, 0, WHITE);
 
 #ifdef DEBUG
         DrawTexture(m_tex, WIDTH-m_tex.width, 0, WHITE);
@@ -123,118 +256,13 @@ public:
         };
         DrawRectangleLinesEx(rect, 1, RED);
 
-        DrawRectangleLinesEx(get_hitbox(), 1, BLACK);
+        DrawRectangleLinesEx(hitbox(), 1, BLACK);
 
-        DrawText(std::format("pos: x: {}, y: {}", trunc(m_position.x), trunc(m_position.y)).c_str(), 0, 0, 50, WHITE);
-        DrawText(std::format("speed: {}:", trunc(m_speed)).c_str(), 0, 50, 50, WHITE);
-        DrawText(std::format("grounded: {}", m_is_grounded ? "yes" : "no").c_str(), 0, 100, 50, WHITE);
+        // DrawText(std::format("pos: x: {}, y: {}", trunc(m_position.x), trunc(m_position.y)).c_str(), 0, 0, 50, WHITE);
+        // DrawText(std::format("speed: {}:", trunc(m_speed)).c_str(), 0, 50, 50, WHITE);
+        // DrawText(std::format("grounded: {}", m_is_grounded ? "yes" : "no").c_str(), 0, 100, 50, WHITE);
 #endif // DEBUG
 
-    }
-
-    void update() {
-        if (m_is_grounded) {
-            m_speed = 0;
-        } else {
-            m_speed += m_gravity * m_get_dt();
-            m_position.y += m_speed * m_get_dt();
-        }
-    }
-
-    void resolve_collisions(const std::span<const Item> items) {
-        m_is_grounded = false;
-
-        for (const auto &item : items) {
-            if (item.m_is_blocking) {
-
-                const auto hitbox = item.m_hitbox;
-                const float delta_ver = m_speed * m_get_dt();
-                const float delta_hor = m_movement_speed * m_get_dt();
-
-                // let the player clip a bit into the floor when grounded, to prevent
-                // oscillation of grounding state
-                // also prevent the player from teleporting down after walking off a ledge
-                const float clip = 1;
-
-                const Rectangle left = {
-                    hitbox.x - delta_hor,
-                    hitbox.y + clip,
-                    delta_hor,
-                    hitbox.height - clip*2,
-                };
-
-                const Rectangle right = {
-                    hitbox.x + hitbox.width,
-                    hitbox.y + clip,
-                    delta_hor,
-                    hitbox.height - clip*2,
-                };
-
-                const float top_delta_mult_factor = 1.3;
-                const Rectangle top = {
-                    // remove some padding from each edge to prevent
-                    // instant teleportation when jumping up and edge
-                    // and moving to the right
-                    hitbox.x + delta_hor*top_delta_mult_factor,
-                    hitbox.y - delta_ver,
-                    hitbox.width - delta_hor*top_delta_mult_factor*2,
-                    delta_ver,
-                };
-
-                const Rectangle bottom = {
-                    hitbox.x,
-                    hitbox.y + hitbox.height,
-                    hitbox.width,
-                    // when the player bumps their head by jumping, the
-                    // speed is negative, therefore it must be inverted
-                    -delta_ver,
-                };
-
-#ifdef DEBUG_COLLISIONS
-                DrawRectangleRec(left,   PURPLE);
-                DrawRectangleRec(right,  PURPLE);
-                DrawRectangleRec(top,    PURPLE);
-                DrawRectangleRec(bottom, PURPLE);
-#endif // DEBUG_COLLISIONS
-
-                if (CheckCollisionRecs(get_hitbox(), top)) {
-                    m_is_grounded = true;
-                    m_position.y = hitbox.y - get_hitbox().height/2.0f + clip;
-                }
-
-                if (CheckCollisionRecs(get_hitbox(), left)) {
-                    m_position.x = hitbox.x - get_hitbox().width/2.0f;
-                }
-
-                if (CheckCollisionRecs(get_hitbox(), right)) {
-                    m_position.x = hitbox.x + hitbox.width + get_hitbox().width/2.0f;
-                }
-
-                if (CheckCollisionRecs(get_hitbox(), bottom)) {
-                    m_speed = 0;
-                }
-
-            }
-
-        }
-
-    }
-
-    void jump() {
-        if (!m_is_grounded) return;
-        m_speed = -m_jumping_speed;
-    }
-
-    void move_right() {
-        m_direction = MovementDirection::Right;
-        m_position.x += m_movement_speed * m_get_dt();
-        walk();
-    }
-
-    void move_left() {
-        m_direction = MovementDirection::Left;
-        m_position.x -= m_movement_speed * m_get_dt();
-        walk();
     }
 
 private:
@@ -258,11 +286,11 @@ static void handle_input(Player &player) {
     }
 
     if (IsKeyDown(KEY_D)) {
-        player.move_right();
+        player.move(MovementDirection::Right);
     }
 
     if (IsKeyDown(KEY_A)) {
-        player.move_left();
+        player.move(MovementDirection::Left);
     }
 
 }
