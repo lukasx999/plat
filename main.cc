41da300dbdef3c77e2a36d448a5be3b5d4e9692e
lukasx999 <116069013+lukasx999@users.noreply.github.com>
2025-06-16 14:13:30 +0200

    textures

--- main.cc
+++ main.cc
@@ -34,33 +34,69 @@ void draw_environment(std::span<const Item> items) {
 
 
 
+
 class Player {
-    Vector2 m_position;
     bool m_is_jumping;
     float m_speed = 0;
     bool m_is_grounded = false;
+    Rectangle m_origin;
+    Vector2 m_position;
+    int m_sprite_index = 0;
     const std::function<float()> m_get_dt;
-    static constexpr int m_size = 100;
+    const Texture2D m_tex;
+    enum class MovementDirection {
+        Left,
+        Right
+    } m_direction = MovementDirection::Left;
+
     static constexpr int m_gravity = 1200;
     static constexpr float m_movement_speed = 600;
     static constexpr float m_jumping_speed = 900;
+    static constexpr Rectangle m_origin_standing { 9, 42, 15, 22 };
+    static constexpr float m_texture_scale = 5;
+    static constexpr int m_sprite_offset = 17;
 
 public:
     Player(std::function<float()> get_dt)
-        : m_position(WIDTH/2.0f, HEIGHT - m_size/2.0f - 500)
-        , m_is_jumping(false)
+        : m_is_jumping(false)
+        , m_origin(m_origin_standing)
+        , m_position(WIDTH/2.0f, HEIGHT - m_origin.height/2.0f - 500)
         , m_get_dt(get_dt)
+        , m_tex(LoadTexture("./characters.png"))
     { }
 
     [[nodiscard]] Rectangle get_hitbox() const {
-        return { m_position.x - m_size/2.0f, m_position.y - m_size/2.0f, m_size, m_size };
+        return {
+            m_position.x - m_origin.width*m_texture_scale/2.0f,
+            m_position.y - m_origin.height*m_texture_scale/2.0f,
+            m_origin.width*m_texture_scale,
+            m_origin.height*m_texture_scale,
+        };
     }
 
     void draw() const {
-        DrawRectangleRec(get_hitbox(), DARKBLUE);
-        DrawCircleV(m_position, 10, RED);
+
+        Rectangle origin = {
+            m_origin.x + m_sprite_index*m_sprite_offset*m_origin.width,
+            m_origin.y,
+            m_origin.width,
+            m_origin.height,
+        };
+
+        switch (m_direction) {
+            case MovementDirection::Left:
+                // flip texture
+                origin.width *= -1;
+                break;
+            case MovementDirection::Right:
+                ;
+                break;
+        }
+
+        DrawTexturePro(m_tex, origin, get_hitbox(), { 0, 0 }, 0, WHITE);
 
 #ifdef DEBUG
+        DrawRectangleLinesEx(get_hitbox(), 1, BLACK);
         DrawText(std::format("pos: x: {}, y: {}", trunc(m_position.x), trunc(m_position.y)).c_str(), 0, 0, 50, WHITE);
         DrawText(std::format("speed: {}:", trunc(m_speed)).c_str(), 0, 50, 50, WHITE);
         DrawText(std::format("jumping: {}", m_is_jumping ? "yes" : "no").c_str(), 0, 100, 50, WHITE);
@@ -70,6 +106,8 @@ public:
     }
 
     void update() {
+
+        walk();
 
         if (m_is_grounded) {
             m_is_jumping = false;
@@ -106,20 +144,19 @@ public:
 
                 if (CheckCollisionRecs(get_hitbox(), top)) {
                     m_is_grounded = true;
-                    m_position.y = hitbox.y - m_size/2.0f;
-                    DrawRectangleRec(top, ORANGE);
+                    m_position.y = hitbox.y - get_hitbox().height/2.0f;
                 }
 
                 if (CheckCollisionRecs(get_hitbox(), left)) {
-                    m_position.x = hitbox.x - m_size/2.0f;
+                    m_position.x = hitbox.x - get_hitbox().width/2.0f;
                 }
 
                 if (CheckCollisionRecs(get_hitbox(), right)) {
-                    m_position.x = hitbox.x + hitbox.width + m_size/2.0f;
+                    m_position.x = hitbox.x + hitbox.width + get_hitbox().width/2.0f;
                 }
 
                 if (CheckCollisionRecs(get_hitbox(), bottom)) {
-                    m_position.y = hitbox.y + hitbox.height + m_size/2.0f;
+                    m_position.y = hitbox.y + hitbox.height + get_hitbox().height/2.0f;
                 }
 
             }
@@ -134,11 +171,25 @@ public:
     }
 
     void move_right() {
+        m_direction = MovementDirection::Right;
         m_position.x += m_movement_speed * m_get_dt();
+        // walk();
     }
 
     void move_left() {
+        m_direction = MovementDirection::Left;
         m_position.x -= m_movement_speed * m_get_dt();
+        // walk();
+    }
+
+private:
+    void walk() {
+        std::println("{}", GetTime());
+        if (trunc(fmod(GetTime()*100, 200)) == 0) {
+            std::println("foo");
+            // m_sprite_index++;
+            // m_sprite_index %= 3;
+        }
     }
 
 };
